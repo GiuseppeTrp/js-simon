4976ba8b16544ecd35891f4302d90e79489a8862
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-11 21:16:14 +0100

    end

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ function countDown(){
 // Ottiene la data e l'ora attuali (del momento in cui si esegue il codice)
 let now = new Date();
 
-// Visualizza la data e l'ora attuali nel formato locale del browser
-document.getElementById("result").innerHTML =  now.toLocaleString();
+
 
 
 // Crea un date che poi viene modificato  per ottenere il giorno desiderato
@@ -12,7 +11,7 @@ let monday = new Date();
 monday.setDate(12); 
 monday.setMonth(1); 
 monday.setFullYear(2024); 
-monday.setHours(9); 
+monday.setHours(10); 
 monday.setMinutes(30); 
 monday.setSeconds(0); 
 monday.setMilliseconds(0); 
@@ -27,30 +26,33 @@ console.log(monday);
 let time = monday.getTime() - now.getTime();
 
 // trasforma il risultato da ms in secondi
-let secondTime = Math.floor( time / 1000);
+let seconds = Math.floor( time / 1000);
 
-// da secondi a giorni
-let days =  Math.floor( secondTime / (24 * 3600));
+
+// da secondi a giorni 
+let days =  Math.floor( seconds / (24 * 3600));
 // da secondi a ore 
-let hours =   Math.floor(secondTime / 3600) ;
+let hours =   Math.floor(seconds / 3600) ;
 
 
-// da secondi a minuti
-let minutes =  Math.floor(secondTime / (3600 / 60));
+// da secondi a minuti 
+let minutes =  Math.floor(seconds / (3600 / 60));
 
 
 
 
-console.log(secondTime);
+console.log(seconds);
 console.log("days",days);
 console.log("hours",hours);
 console.log("minuti",minutes);
 
 
 
-
+document.getElementById("result").innerHTML = " Giorni: " + days + "<br>  Ore: " + hours + "<br>  Minuti: " + minutes + "<br>  Secondi: " + seconds;
+document.getElementById("monday").innerHTML = monday.toDateString();
 
 
 }
 
 let interval = setInterval(countDown, 1000);
+
