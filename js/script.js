8d68beb139e993f7dc35b5ca50f010567dcf99d8
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-11 20:01:58 +0100

    added monday variable

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ let now = new Date();
 
 // Visualizza la data e l'ora attuali nel formato locale del browser
 document.getElementById("result").innerHTML =  now.toLocaleString();
+
+
+// Crea un date che poi viene modificato  per ottenere il giorno desiderato
+let monday = new Date(); 
+monday.setDate(12); 
+monday.setMonth(1); 
+monday.setFullYear(2024); 
+monday.setHours(9); 
+monday.setMinutes(30); 
+monday.setSeconds(0); 
+monday.setMilliseconds(0); 
+
+console.log(monday); 
